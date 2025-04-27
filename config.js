549332ba898ebc864537d27daf9f45acca55eb57
Braan7 <braanx7@gmail.com>
2025-04-27 12:51:19 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.jadi = 'BarbozaJadiBot'
+global.jadi = 'BraanJadiBot'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
