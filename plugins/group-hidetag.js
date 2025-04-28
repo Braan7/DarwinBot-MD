0fec699b6ec64e04930f1e3168a1e1044a92d30f
Braan7 <braanx7@gmail.com>
2025-04-28 17:18:05 -0500

    Actualizar group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -26,7 +26,7 @@ const handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     const quoted = m.quoted ? m.quoted : m;
     const mime = (quoted.msg || quoted).mimetype || '';
     const isMedia = /image|video|sticker|audio/.test(mime);
-    const watermark = '\n\n> 𝐁𝐫𝐚𝐚𝐧𝐁𝐨𝐭 🥷🏼';
+    const watermark = '\n\n> Streaming Anilu';
 
     if (isMedia) {
       const mediax = await quoted.download?.();
