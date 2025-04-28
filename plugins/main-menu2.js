bc13f59f999558ebac056e16217a824e11c3aeae
Braan7 <braanx7@gmail.com>
2025-04-28 16:41:04 -0500

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { isPrems, conn }) => {
-let img = 'https://qu.ax/cATgq.jpg' 
+let img = 'https://qu.ax/FHmRy.jpg' 
 let texto = `*☁️ _M E N U - A U D I O S_ ☁️*
 
 ° _Tunometecabrasaramambiche_.
