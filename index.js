6784625f58f0ef65cd070c980e33b20dbd218621
Braan7 <braanx7@gmail.com>
2025-04-28 20:18:37 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando AniluBot-MD ✰');
+console.log('\n✰ Iniciando DarwinBot-MD ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
